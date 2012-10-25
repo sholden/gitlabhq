09b1305823b5ff8b0a519415df689a2fd35dd09f
Scott Holden <sholde4@gmail.com>
2012-10-24 21:51:55 -0700

    Update app/models/note.rb
    Without lambdas, Date.today will be evaluated in the class body.
    For it to have a running scope of last week etc, it will need to
    be evaluated each time the scope is called.

--- app/models/note.rb
+++ app/models/note.rb
@@ -24,8 +24,8 @@ class Note < ActiveRecord::Base
 
   # Scopes
   scope :common, where(noteable_id: nil)
-  scope :today, where("created_at >= :date", date: Date.today)
-  scope :last_week, where("created_at  >= :date", date: (Date.today - 7.days))
+  scope :today, ->{ where("created_at >= :date", date: Date.today) }
+  scope :last_week, ->{ where("created_at  >= :date", date: (Date.today - 7.days)) }
   scope :since, ->(day) { where("created_at  >= :date", date: (day)) }
   scope :fresh, order("created_at ASC, id ASC")
   scope :inc_author_project, includes(:project, :author)
